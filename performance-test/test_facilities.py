5f8ce5c0b29d4a3cac2bf4bb2eb7f9557adb3018
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2016-02-17 17:22:51 -0600

    clean up testing function, adding keyword relevant to this testing function

--- performance-test/test_facilities.py
+++ performance-test/test_facilities.py
@@ -4,49 +4,7 @@ import os
 import sqlite3
 import tables
 import numpy as np
-from tools import safe_call 
-
-def change_input(ref_input, growth_factor):
-    """Changes a growth factor of the Growth Region of the input file.
-
-    Args:
-        ref_input: The path to the reference input file 
-	growth_factor: A tuple of (growth factor, initial y value).
-
-    Returns:
-        A path to a file with an updated growth factor.
-    """
-    growth_factor = growth_factor[0]
-    y_init = growth_factor[1]
-
-    # A file to be created
-    new_sim = ref_input.split(".xml")[0] + "_" + str(growth_factor[0]) + \
-              "_" + ".xml"
-    sim = open(new_sim, "w")
-    ref = open(ref_input, "r")
-    
-    # Change the growth factor value in the input file
-    # need more robust method for initial y value
-    for f in ref:
-        if f.count("params"):
-            f = f.split("<")[0] + "<params>" + str(growth_factor) + \
-                " " + str(y_init) + "</params>\n"
-        # write the rest of the file
-        sim.write(f)
-    
-    # Closing open files
-    ref.close()
-    sim.close()
-
-    return new_sim
-
-def rm_out(outfile):
-    """
-    Deletes an output simulation file
-    """
-    # Removes output files
-    if os.path.exists(outfile):
-        os.remove(outfile)
+from tools import safe_call, rm_file, change_input
 
 def test_facilities():
     """
@@ -57,19 +15,24 @@ def test_facilities():
     # Simulation input file for performance testing
     ref_input = "./testing.xml"
     # Growth factors to change the number of facilities in each sim
-    growth_factors = [(0, 10000), (0.5, 10000), (1, 10000)]
+    growth_factors = ["0 10000", "0.5 10000", "1 10000"]
     # Output files
     #outfiles = ["output_temp.h5", "output_temp.sqlite"]
     outfiles = ["output_temp.sqlite"]
-
+    searchfor="GRUYERE"
     for gf in growth_factors:
         for outfile in outfiles:
-	    rm_out(outfile)
-	    sim_input = change_input(ref_input, gf)
-            cmd = ["cyclus", "-o", outfile, "--input-file", sim_input]
+	    rm_file(outfile)
+	    db = outfile.split(".sqlite")[0] + "_" + str(gf) + \
+	         ".sqlite"
+	    sim_input = change_input(ref_input, gf, searchfor)
+            cmd = ["cyclus", "-o", db, "--input-file", sim_input]
             safe_call(cmd)
-	    
+	    rm_file(sim_input)	    
+
 	    # Cym processing stuff goes here
 	    print('something')
+	    rm_file(db)
     return
 
+test_facilities()
