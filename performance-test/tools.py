7d4e3c89369f906c01c16ffd116e239fdc309884
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2016-02-24 19:58:47 -0600

    dont need separate defaults function with the template

--- performance-test/tools.py
+++ performance-test/tools.py
@@ -3,53 +3,30 @@ import os
 import subprocess
 from jinja2 import FileSystemLoader, Environment, Template
 
-def fill_defaults(ref_input):
-    """Fills in default values for non-varying input params.
+def change_input(ref_input, value, parameter):
+    """Changes a parameter in the input file.
 
     Args:
-        ref_input: The path to the reference input file 
+        ref_input: The path to the reference xml input file 
+        value: A text value to be added into the xml template
+        parameter: A text indicator in the template to replace a parameter
 
     Returns:
-        A path to a file ready to run in a simulation.
+        A path to a file with an updated input file parameter.
     """
     templateLoader = FileSystemLoader(searchpath="./")
     templateEnv = Environment(loader=templateLoader)
     defaults = {'growrate': '0 10000', 'dt': '2629846', 'assemsize': '20000', \
                 'cycletime': '18', 'outrecipe': 'three'}
     ref = templateEnv.get_template(ref_input)
+    defaults[parameter]=value
     sim = ref.render(defaults=defaults)
-    new_sim = 'new_sim.xml'
+    new_sim = ref_input.split(".xml")[0] + "_" + str(parameter) + ".xml"
     sim_file = open(new_sim, "w")
     for line in sim:
         sim_file.write(line)
-    return new_sim
+    sim_file.close()
 
-def change_input(ref_input, parameter, search_text):
-    """Changes a parameter in the input file.
-
-    Args:
-        ref_input: The path to the reference input file 
-        parameter: A text value to replace the search text
-        search_text: A text indicator of where to replace an input parameter
-
-    Returns:
-        A path to a file with an updated input file parameter.
-    """
-    # A file to be created
-    new_sim = ref_input.split(".xml")[0] + "_" + str(parameter) + ".xml"
-    sim = open(new_sim, "w")
-    ref = open(ref_input, "r")
-    
-    # Change the value in the input file
-    for f in ref:
-        if search_text in f:
-            sim.write(f.replace(search_text, parameter))
-        else:
-            sim.write(f)
-    
-    # Closing open files
-    ref.close()
-    sim.close()
     return new_sim
 
 def rm_file(file_path):
